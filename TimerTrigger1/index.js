893fa4550c48862337286e807053d22eb701820d
Masiha <v-maskal@microsoft.com>
2021-04-16 23:17:59 +0000

    fm

--- TimerTrigger1/index.js
+++ TimerTrigger1/index.js
@@ -3,7 +3,7 @@ module.exports = async function (context, myTimer) {
     
     if (myTimer.IsPastDue)
     {
-        context.log('JavaScript is running late!');
+        context.log('JavaScript is running late!!');
     }
     context.log('JavaScript timer trigger function ran!', timeStamp);   
 };
